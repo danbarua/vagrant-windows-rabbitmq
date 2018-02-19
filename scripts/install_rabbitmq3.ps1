60e4f35995678f19802c6263d37f12a6ae69a918
Nathan Brown <nathan@nkbrown.us>
2018-02-19 09:26:59 -0700

    Update install_rabbitmq3.ps1
    Correct port for AMQP over SSL.

--- scripts/install_rabbitmq3.ps1
+++ scripts/install_rabbitmq3.ps1
@@ -3,5 +3,5 @@ Copy-Item C:\Windows\.erlang.cookie C:\Users\vagrant\.erlang.cookie -Force
 
 Write-Host "Enabling Firewall ports"
 New-NetFirewallRule -DisplayName "Allow Port 5672 (AMQP)" -Direction Inbound -LocalPort 5672 -Protocol TCP -Action Allow
-New-NetFirewallRule -DisplayName "Allow Port 5671 (AMQP over SSL)" -Direction Inbound -LocalPort 5672 -Protocol TCP -Action Allow
+New-NetFirewallRule -DisplayName "Allow Port 5671 (AMQP over SSL)" -Direction Inbound -LocalPort 5671 -Protocol TCP -Action Allow
 New-NetFirewallRule -DisplayName "Allow Port 15672 (RabbitMQ Management)" -Direction Inbound -LocalPort 15672 -Protocol TCP -Action Allow
